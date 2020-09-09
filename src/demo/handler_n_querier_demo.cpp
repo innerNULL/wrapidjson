93756651e86710a0731c5931cefd4ad64520c510
innerNULL <lyt697@outlook.com>
2020-09-09 13:28:30 +0800

    Rearrange and add demo.

--- src/demo/handler_n_querier_demo.cpp
+++ src/demo/handler_n_querier_demo.cpp
@@ -19,9 +19,10 @@
 
 #include "wrapidjson/checker/checker.h"
 #include "wrapidjson/handler/handler.h"
+#include "wrapidjson/querier/querier.h"
 
 
-int32_t handler_demo() {
+int32_t handler_n_querier_demo() {
   std::cout << "=======handler_demo=======" << std::endl;
   std::cout << "-------case0-------" << std::endl;
   rapidjson::Document case0_var0 = wrapidjson::json_obj_init(true);
@@ -49,11 +50,20 @@ int32_t handler_demo() {
   wrapidjson::json_insert_num_array<float>(case1_var0, "fake6", {-1.25, 0.25, 1.25}, true);
   wrapidjson::json_insert_num_array<int32_t>(case1_var0, "fake3", {10, 50, 100}, true);
   std::cout << wrapidjson::json_obj2string(case1_var0) << std::endl;
+
+  std::cout << "-------case2-------" << std::endl;
+  std::vector<float> case2_var0;
+  std::vector<int32_t> case2_var1;
+  wrapidjson::json_num_array2std_vec<float>(case2_var0, case1_var0, "fake6");
+  wrapidjson::json_num_array2std_vec<int32_t>(case2_var1, case1_var0, "fake6");
+  for (int32_t i = 0; i < case2_var1.size(); ++i) {
+    std::cout << case2_var0[i] << ", " << case2_var1[i] << std::endl;
+  }
   return 0;
 }
 
 
 int main(int argc, char *argv[]) {
-  handler_demo();
+  handler_n_querier_demo();
   return 0;
 }
